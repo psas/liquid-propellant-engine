6060d3f435e6ad46eb0b0f0f3a2014ede9545df0
7deeptide <7deeptide@gmail.com>
2016-05-11 15:01:49 -0700

    getting closer to fully working w/ linear test geometry

--- Nozzle_Construction/ht.py
+++ Nozzle_Construction/ht.py
@@ -8,7 +8,7 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-from math import sqrt
+from math import sqrt, pi
 
 #jupyter magic words
 #%config InlineBackend.figure_formats=['svg']
@@ -21,6 +21,8 @@ mdot = 1 #choked mass flow rate (e.g. mdot at throat)
 R =  1 #specific gas constant (from CEA)
 p_exit = 14.4 #exit pressure (assuming optimal expansion)
 p_chamber_ns = 375 # or whatever, total chamber pressure
+r0 = 1
+rt = 0.3
 
 #init. arrays
 n = 0 
@@ -31,25 +33,27 @@ V = [R*T[n]/p[n]] #specific vol
 v = [0] #velocity
 Mach = [0] #Mach number
 A = [mdot*V[n]/v[n]] #cross-sectional area
-x = [] #length
+x = [0] #length
 #Also add Pr, Re, Nu, h, etc, etc...
 
 #start loop for flow properties
 n = 1
 while n < it:
-	V.append(V[n-1]*(p[n-1]/p[n])**(1/k))
-	T.append(T[n-1]*(p[n]/p[n-1])**((k-1)/k))
-	v.append(sqrt(2*k*R*T[n-1]/(k-1)*(1-(p[n]/p[n-1])**((k-1)/k))))
+	V.append(V[0]*(p[0]/p[n])**(1/k))
+	T.append(T[0]*(p[n]/p[0])**((k-1)/k))
+	v.append(sqrt(2*k*R*T[0]/(k-1)*(1-(p[n]/p[0])**((k-1)/k))))
 	Mach.append(v[n]/(sqrt(k*R*T[n])))
-	A.append(mdot*V[n]/v[n])
-	#Also add Pr, Re, Nu, h, etc, etc...
-#	if Mach[n] < 1:
-#		x.append("some defined function of A")
-#	else:
-#		x.append("some other defined function of A")	
+
+	if Mach[n] < 1:
+		A.append(mdot*V[n]/v[n])
+		x.append(-sqrt(A[n]/pi) + r0)
+		xn = x[n]
+	else:
+		A.append(mdot*V[n]/v[n])
+		x.append((sqrt(A[n]/pi) - rt)/2 + xn)	
 	n += 1
 
-plt.plot(A, T) #really should be a plot vs x
+plt.plot(x, p) #really should be a plot vs x
 plt.title("yada yada vs. thingamajig")
 plt.ylabel("whichever variable")
 plt.xlabel("x [in or m or whatever]")
